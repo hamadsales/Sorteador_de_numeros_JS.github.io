6029203afc265ab8eff092c0541aea784bb3419b
Hamad Sales <68565491+hamadsales@users.noreply.github.com>
2026-05-17 14:33:24 -0300

    Update script.js
    update de código com exceções

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const variavel = {}
-var numeroAleatorio;
+var posicao;
 let numeros = []
 
 function gerarvariaveis(nome, tag, id) {
@@ -13,48 +13,86 @@ gerarvariaveis('sortear', 'button', 'sortear')
 gerarvariaveis('reiniciar', 'button', 'reiniciar')
 gerarvariaveis('resultado', 'p', 'texto-resultado')
 
-function gerarNumeroAleatorio(min, max) {
-    numeroAleatorio = Math.floor(Math.random() * (max - min + 1)) + min // Math.trunc retorna a parte inteira do número
+function gerarPosicao(min, max) {
+    posicao = Math.floor(Math.random() * (max - min + 1)) + min // Math.trunc retorna a parte inteira do número
 }
 
-function adicionarNumeros(numeroAleatorioAdicionado) {
-    for (let i = 0; i <= numeroAleatorioAdicionado; i++) {
+function adicionarNumeros(numero_inicio, numero_fim) {
+    for (let i = numeroInicial; i <= numeroFinal; i++) {
         numeros.push(i)//adiciona os valores de i ao Array numeros
     }
 }
 
+function sorteadorDeNumeros() {
+
+    if (variavel.resultado.textContent == 'Números sorteados: nenhum até agora') {
+        variavel.resultado.innerHTML = ` `
+    }
+    
+    variavel.resultado.innerHTML += `${numeros[posicao]} ` // x+=1 é o mesmo que  x = x + 1, adicionamos sem apagar
+    numeros.splice(posicao, 1) // .splice(x,y) vá até a posição X e remova Y elementos"
+    variavel.reiniciar.style.backgroundColor = '#8b1eba'
+
+}
+
 function limparCampos() {
+
     variavel.numeroInicial.value = ``
     variavel.numeroFinal.value = ``
     variavel.quantidadeNumeros.value = ``
     variavel.resultado.innerHTML = ``
+
 }
 
-function sorteadorDeNumeros() {
+function verificador(){
+
     numeroInicial = Number(variavel.numeroInicial.value)
     numeroFinal = Number(variavel.numeroFinal.value)
+    quantidadeNumeros = Number(variavel.quantidadeNumeros.value)
 
-    gerarNumeroAleatorio(numeroInicial, numeroFinal)
-    adicionarNumeros(numeroAleatorio)
-
-    if(variavel.resultado.textContent == 'Números sorteados: nenhum até agora'){
-        variavel.resultado.innerHTML = ` `
+    if(!variavel.numeroFinal.value || !variavel.numeroInicial.value || !variavel.quantidadeNumeros.value){
+        variavel.resultado.textContent = `Preencha todos os campos`
+        return false
     }
 
-    variavel.resultado.innerHTML += `${numeros[numeroAleatorio]} ` // x+=1 é o mesmo que  x = x + 1
-    numeros.splice(1,numeroAleatorio) // remove 1 item na posição numero aleatório
-    variavel.reiniciar.style.backgroundColor = '#8b1eba'
+    if (numeroFinal <= numeroInicial) {
+        limparCampos()
+        variavel.resultado.textContent = `O numero inicial deve ser maior que o número final`
+        return false
+    }
+    if(quantidadeNumeros > ((numeroFinal-numeroInicial)+1)){
+        limparCampos()
+        variavel.resultado.textContent = `A quantidade de números sorteador deve ser menor que a quantidade de números do sorteio`
+        return false
+    }
+    return true
+
 }
 
-function jutsu(){
+function jutsu() {
+
+    if(verificador() == false){
+        return 
+    }
+    variavel.resultado.textContent = ` `
+
+    numeroInicial = Number(variavel.numeroInicial.value)
+    numeroFinal = Number(variavel.numeroFinal.value)
     quantidadeNumeros = Number(variavel.quantidadeNumeros.value)
-    for(var j = 0;j<quantidadeNumeros;j++){
+
+    adicionarNumeros(numeroInicial, numeroFinal)
+
+    for (var j = 0; j < quantidadeNumeros; j++) {
+        gerarPosicao(0, numeros.length - 1)
         sorteadorDeNumeros()
     }
+    numeros = []
+
 }
+
 function reiniciar() {
     numeros = []
     variavel.reiniciar.style.backgroundColor = '#333'
-    numeroAleatorio;
+    posicao;
     limparCampos()
 }
